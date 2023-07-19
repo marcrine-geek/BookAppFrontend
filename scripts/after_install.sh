7501274ad618ca3be4e95144f80b96d43c33f62f
marcrine-geek <marcrinemm@gmail.com>
2023-07-19 17:44:48 +0300

    updates

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # navigate to app folder
-cd /app
+cd app
 
 # install dependencies
 npm install
-sudo npm install --save react react-dom react-scripts
+npm start
 npm run build
 cp -r build/* /var/www/html
 npm install pm2 -g
