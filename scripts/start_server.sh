49d047ababc931da14d0981a5024c438faee247d
marcrine-geek <marcrinemm@gmail.com>
2023-07-19 16:44:57 +0300

    updates

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# navigate to app folder
-cd app
+# # navigate to app folder
+# cd app
 
-# initial startup by running react-script "start", name process "marketing"
-# --watch watches and restarts if files change
-npm start
-pm2 start
-pm2 startup
-pm2 save
-pm2 restart all
+# # initial startup by running react-script "start", name process "marketing"
+# # --watch watches and restarts if files change
+# npm start
+# pm2 start
+# pm2 startup
+# pm2 save
+# pm2 restart all
+
+systemctl restart nginx
