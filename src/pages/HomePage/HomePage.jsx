b32069518fe0e9a736225e8e02e37aaaf664e790
marcrine-geek <marcrinemm@gmail.com>
2023-07-19 10:15:37 +0300

    updates

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -18,6 +18,7 @@ const HomePage = () => {
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae
 						sapiente quibusdam consequatur perspiciatis facere laboriosam non
 						nesciunt at id repudiandae, modi iste? Eligendi, rerum!
+						LOREMMMMMMMMMM WORKING
 					</p>
 				</div>
 			</header>
