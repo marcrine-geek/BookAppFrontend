cefce92e9f264314f875e8d7744ff556debde5a8
marcrine-geek <marcrinemm@gmail.com>
2023-07-19 16:15:08 +0300

    updates

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,7 +8,6 @@ npm install
 
 # install create-react-app and react-scripts
 # without react-scripts application cannot be started
-npm install -g create-react-app
-
+npm install --save react react-dom react-scripts
 # install pm2 process manager
 npm install pm2 -g
