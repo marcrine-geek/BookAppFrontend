6cfd38e289e5771a166cd083228609267ef47084
marcrine-geek <marcrinemm@gmail.com>
2023-07-19 16:28:57 +0300

    updates

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,15 +6,7 @@ cd app
 # initial startup by running react-script "start", name process "marketing"
 # --watch watches and restarts if files change
 npm start
-
-# auto restart server if shut down
 pm2 start
-
-# freeze process list for automatic respawn
-pm2 save --force
-
-# restart all processes - necessary to do this again?
 pm2 startup
-sudo systemctl enable pm2-ubuntu
-
+pm2 save
 pm2 restart all
