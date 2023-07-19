c28842b37ed0b7e58820defe26927a19c9e22dd8
marcrine-geek <marcrinemm@gmail.com>
2023-07-19 19:59:01 +0300

    updates

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,5 +7,6 @@ cd /app
 apt-get install curl -y
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 apt-get install nodejs -y
+apt-get install npm -y
 apt install nginx -y
 ufw allow 'Nginx HTTP'
